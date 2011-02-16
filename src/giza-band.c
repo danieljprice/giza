27972ed8f0bd535973eb34f744554a73a6b2de49
danielprice <danielprice@952f94de-64c9-43ce-8081-c12abd9885bf>
2011-02-16 11:22:14 +0000

    giza_band stuff moved to giza_band routine where possible; implemented all 7 pgplot band styles plus additional circle band style; better docs for giza_band
    git-svn-id: https://svn.code.sf.net/p/giza/code@26 952f94de-64c9-43ce-8081-c12abd9885bf

--- src/giza-band.c
+++ src/giza-band.c
@@ -12,15 +12,18 @@
  *  a) You must cause the modified files to carry prominent notices
  *     stating that you changed the files and the date of any change.
  *
- * Copyright (C) 2010 James Wetter. All rights reserved.
- * Contact: wetter.j@gmail.com
+ * Copyright (C) 2010-2011 James Wetter and Daniel Price. All rights reserved.
+ * Contact: wetter.j@gmail.com, daniel.price@monash.edu
  *
  */
 
 #include "giza-private.h"
 #include "giza-io-private.h"
 #include "giza-drivers-private.h"
+#include "giza-band-private.h"
 #include <giza.h>
+#include "math.h"
+#define GIZA_MAX_BAND_MODES 8
 
 /**
  * Interactive: giza_band
@@ -29,7 +32,7 @@
  * relative to an anchor point
  *
  * Input:
- *  -mode     :- selects the mode to draw the band.
+ *  -mode     :- selects the type of shape to draw during input
  *  -moveCurs :- if 1 the cursor is moved to (x, y),
  *               if 0 the cursor is not moved.
  *  -xanc     :- the x-coord of the anchor point.
@@ -41,12 +44,31 @@
  * Return:
  *  -1 :- The device has no cursor
  *  -0 :- The call was successful
+ *
+ * Modes:
+ *  -0 :- None, behaves like giza_get_key_press
+ *  -1 :- Straight line drawn from anchor point
+ *  -2 :- Hollow rectangle
+ *  -3 :- Two horizontal lines
+ *  -4 :- Two vertical lines
+ *  -5 :- Single horizontal line, ignores anchor point
+ *  -6 :- Single vertical lines, ignores anchor point
+ *  -7 :- Cross hair, ignores anchor point
+ *  -8 :- Circle centred on anchor point
+ *
+ * See Also: giza_get_key_press
  */
 int
 giza_band (int mode, int moveCurs, double xanc, double yanc, double *x, double *y, char *ch)
 {
   if(!_giza_check_device_ready ("giza_band"))
     return 1;
+
+  if (mode < 0 || mode > GIZA_MAX_BAND_MODES) 
+    {
+      _giza_error("giza_band","Invalid band mode selected");
+      return 2;
+    }
 
   return _giza_get_key_press (mode, moveCurs, xanc, yanc, x, y, ch);
 }
@@ -72,4 +94,108 @@ giza_band_float (int mode, int moveCurs, float xanc, float yanc, float *x, float
   return success;
 }
 
+/*
+ * Draws over the old band and draws the new one.
+ */
+void
+_giza_refresh_band (int mode, int x1, int y1, int x2, int y2)
+{
+  if (mode <= 0 || mode > GIZA_MAX_BAND_MODES) return;
+
+  // Draw over the old band
+  cairo_paint (Band.restore);
+ 
+/* 
+  // set clipping
+  int topleftx = x1 - 10;
+  int toplefty = y1 - 10;
+  int bottomrightx = x2 + 10;
+  int bottomrighty = y2 + 10;
+  if (x2 - x1 <= 0)
+    {
+      topleftx = x2 - 10;
+      bottomrightx = x1 + 10;
+    }
+  if (y2 - y1 <= 0)
+    {
+      toplefty = y2 - 5;
+      bottomrighty = y1 + 5;
+    }
+  cairo_rectangle (Band.box, x1, y1, x2 - x1, y2 - y1);
+  cairo_rectangle (Band.restore, x1, y1, x2 - x1, y2 - y1);
+  cairo_reset_clip (Band.box);
+  cairo_reset_clip (Band.restore);
+  cairo_clip (Band.box);
+  cairo_clip (Band.restore);
+ */
+  switch (mode)
+    {
+      case 1: // Straight line
+       // Draw the band
+	cairo_move_to (Band.box, x1, y1);
+	cairo_line_to (Band.box, x2, y2);
+	cairo_stroke (Band.box);
+	break;
+      case 2: // empty rectangle
+        // Draw the band
+        cairo_rectangle (Band.box, x1, y1, x2 - x1, y2 - y1);
+        cairo_stroke (Band.box);
+	break;
+      case 3: // Two horizontal lines
+        cairo_move_to (Band.box, 0., y1);
+        cairo_line_to (Band.box, Band.maxWidth, y1);
+        cairo_move_to (Band.box, 0., y2);
+        cairo_line_to (Band.box, Band.maxWidth, y2);
+        cairo_stroke (Band.box);
+	break;
+      case 4: // Two vertical lines
+        cairo_move_to (Band.box, x1, 0.);
+        cairo_line_to (Band.box, x1, Band.maxHeight);
+        cairo_move_to (Band.box, x2, 0.);
+        cairo_line_to (Band.box, x2, Band.maxHeight);
+        cairo_stroke (Band.box);
+	break;
+      case 5: // Single, horizontal line
+        cairo_move_to (Band.box, 0., y2);
+        cairo_line_to (Band.box, Band.maxWidth, y2);
+        cairo_stroke (Band.box);
+	break;
+      case 6: // Single, vertical line
+        cairo_move_to (Band.box, x2, 0.);
+        cairo_line_to (Band.box, x2, Band.maxHeight);
+        cairo_stroke (Band.box);
+	break;
+      case 7: // Crosshair
+        cairo_move_to (Band.box, 0., y2);
+        cairo_line_to (Band.box, Band.maxWidth, y2);
+        cairo_move_to (Band.box, x2, 0.);
+        cairo_line_to (Band.box, x2, Band.maxHeight);
+        cairo_stroke (Band.box);
+	break;
+      case 8: // Circle
+        cairo_arc (Band.box, x1, y1, sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)), 0., 2 * M_PI);
+        cairo_stroke (Band.box);
+	break;
+      default:
+        /* do not warn here (too verbose), instead warn in giza_band */
+	break;
+    }
+}
+
+/*
+ * Cleans up the stuff associated with the band
+ */
+void
+_giza_destroy_band (int mode)
+{
+  if (mode == 0) return;
+
+  // Free memory
+  cairo_destroy (Band.restore);
+  cairo_destroy (Band.box);
+  cairo_surface_destroy (Band.onscreen);
+
+  // remove the last band
+  giza_flush_device ();
+}
 
