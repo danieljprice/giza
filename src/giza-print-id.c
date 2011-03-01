8d1cccedc630ae2a24b4f49797dd60ee2605edf1
danielprice <danielprice@952f94de-64c9-43ce-8081-c12abd9885bf>
2011-03-01 22:23:21 +0000

    giza-print-id now working properly
    git-svn-id: https://svn.code.sf.net/p/giza/code@73 952f94de-64c9-43ce-8081-c12abd9885bf

--- src/giza-print-id.c
+++ src/giza-print-id.c
@@ -20,6 +20,7 @@
 
 #include "giza-private.h"
 #include "giza-window-private.h"
+#include "giza-transforms-private.h"
 #include <giza.h>
 #include <string.h>
 #include <unistd.h>
@@ -38,24 +39,43 @@ giza_print_id (void)
     {
       return;
     }
+  /* get user id */
   char *userid = getlogin();
   if (userid == NULL) userid = "";
   
+  /* get current date and time */
   struct tm *current;
   time_t now;
   time(&now);
   current = localtime(&now);
+
+  /* format the date and time into a string */
   char date[20];
-  strftime(date,20," %e-%h-%Y %H:%M",current);
+  strftime(date,sizeof(date)," %e-%h-%Y %H:%M",current);
   char *string = strcat(userid,date);
   
   double ch;
   giza_get_character_height(&ch);
   giza_set_character_height(0.6);
 
-  //double dx,dy,x,y;
-  //giza_qtextlen(GIZA_UNITS_WORLD,string,dx,dy);
-  giza_text(Win.xmax,Win.ymin,string);
+  /* query dimensions of the surface and size of string */
+  double dx,dy,x,y,width,height;
+  giza_get_paper_size(GIZA_UNITS_DEVICE,&width,&height);
+  giza_qtextlen(GIZA_UNITS_DEVICE,string,&dx,&dy);
   
+  /* place text at bottom right corner of surface */
+  x = width - 0.1*dx;
+  y = height - dy;
+
+  /* convert this to world coordinates */
+  int oldtrans = _giza_get_trans();
+  _giza_set_trans(GIZA_TRANS_WORLD);
+  cairo_device_to_user (context, &x, &y);
+  
+  /* print text at location */
+  giza_ptext(x,y,0.,1.,string);
+  
+  /* reset settings */
+  _giza_set_trans(oldtrans);
   giza_set_character_height(ch);
 }
