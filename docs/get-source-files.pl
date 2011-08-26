da2f9a17a4fae3777bf749cc58f5d4ee861a2716
danielprice <danielprice@952f94de-64c9-43ce-8081-c12abd9885bf>
2011-08-26 07:38:07 +0000

    script for getting list of source files for Makefile
    git-svn-id: https://svn.code.sf.net/p/giza/code@194 952f94de-64c9-43ce-8081-c12abd9885bf

---
+++ docs/get-source-files.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+#
+# @(#) Perl script to get current source files for Makefile
+#
+# @(#) (c) Daniel Price, Jan 2011, daniel.price@monash.edu
+#
+use Text::Wrap;
+
+$Text::Wrap::columns=80;
+$Text::Wrap::separator=" \\\n";
+print wrap('PHEADERS= ','          ',@ARGV);
+print "\n";
