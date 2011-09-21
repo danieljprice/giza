d106edcca10d516506a7048caaff242bca55279a
danielprice <danielprice@952f94de-64c9-43ce-8081-c12abd9885bf>
2011-09-21 00:14:55 +0000

    minor edit to fortran-params script
    git-svn-id: https://svn.code.sf.net/p/giza/code@249 952f94de-64c9-43ce-8081-c12abd9885bf

--- docs/get-fortran-params.pl
+++ docs/get-fortran-params.pl
@@ -9,7 +9,7 @@ open(FILE,"<../src/giza-shared.h");
 while(<FILE>) {
   if (m/define\s\S*\s*([0-9]*)/ && !m/GIZA_LEFT_CLICK/) {
     ($var, $val) = m/define\s(\S*)\s*([0-9]*)/;
-    my $lvar = lc($var);
+    my $lvar = lc($var); #sprintf("%-27s",lc($var));
     print "  integer, parameter, public :: $lvar = $var\n";
   }
 }
