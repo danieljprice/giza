9f7c3dee4ff9ceffa560af7edfdb097104abf440
danielprice <danielprice@952f94de-64c9-43ce-8081-c12abd9885bf>
2012-11-02 04:06:48 +0000

    various line palettes added; can be selected with GIZA_PALETTE environment variable
    git-svn-id: https://svn.code.sf.net/p/giza/code@349 952f94de-64c9-43ce-8081-c12abd9885bf

--- src/giza-colour-palette.c
+++ src/giza-colour-palette.c
@@ -25,6 +25,8 @@
 #include "giza-colour-private.h"
 #include "giza-private.h"
 #include <giza.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Settings: giza_set_colour_palette
@@ -43,7 +45,8 @@
  * Available palette options:
  *  -0 or GIZA_COLOUR_PALETTE_DEFAULT :- default giza palette
  *  -1 or GIZA_COLOUR_PALETTE_PGPLOT  :- default PGPLOT palette
- *
+ *  -2 to 4 :- various experimental line palettes
+ *  -5 :- colourblind safe palette from http://jfly.iam.u-tokyo.ac.jp/color/
  * See Also: giza_set_colour_index, giza_set_colour_table
  *
  */
@@ -52,12 +55,24 @@ giza_set_colour_palette (int palette)
 {
   int nlinecolours;
 
+  char *text = getenv ("GIZA_PALETTE");
+  if(text)
+    {
+      int mypalette;
+      if ( sscanf(text, "%i", &mypalette) == 1 )
+         {
+            palette = mypalette;
+         }
+    }
+
+  /* set foreground/background colours independently */
+  giza_set_colour_representation (0,1.0,1.0,1.0);  /* white */
+  giza_set_colour_representation (1,0.0,0.0,0.0);  /* black */
+
   switch(palette)
   {
     case GIZA_COLOUR_PALETTE_PGPLOT:
     /* Installs the standard PGPLOT colour table */
-       giza_set_colour_representation (0,1.0,1.0,1.0);	/* white */
-       giza_set_colour_representation (1,0.0,0.0,0.0);	/* black */
        giza_set_colour_representation (2,1.0,0.0,0.0);
        giza_set_colour_representation (3,0.0,1.0,0.0);
        giza_set_colour_representation (4,0.0,0.0,1.0);
@@ -73,6 +88,46 @@ giza_set_colour_palette (int palette)
        giza_set_colour_representation (14,0.333,0.333,0.333);
        giza_set_colour_representation (15,0.667,0.667,0.667);
        nlinecolours = 16;
+       break;
+    case 2:
+       giza_set_colour_representation (2, 1., 0., 0.);   /* red */
+       giza_set_colour_representation (3, 0., 0.25, 1.); /* blue */
+       giza_set_colour_representation (4, 0.5, 0., 0.5); /* purple */
+       giza_set_colour_representation (5, 0.02,0.93,0.93);  /* cyan */
+       giza_set_colour_representation (6, 0., 0.5, 0.);  /* green */
+       giza_set_colour_representation (7, 0.5625, 0.554688, 0.011719); /* mustard */
+       giza_set_colour_representation (8, 0.3,0.2,0.15);  /* brown */
+       giza_set_colour_representation (9,1.0,0.5,0.0); /* orange */
+       giza_set_colour_representation_rgb (10, 249, 223, 11); /* desert sun */
+       nlinecolours = 11;
+       break;
+    case 3:
+       giza_set_colour_representation_rgb (2, 85, 98, 112); /* mighty slate */
+       giza_set_colour_representation_rgb (3, 78, 205, 196); /* pacifica */
+       giza_set_colour_representation_rgb (4, 199, 244, 100);  /* apple chic */
+       giza_set_colour_representation_rgb (5, 255, 107, 107); /* cheery pink */
+       giza_set_colour_representation_rgb (6, 196, 77, 88); /* grandma's pillow */
+       nlinecolours = 7;
+       break;
+    case 4:
+       giza_set_colour_representation_rgb (2, 196, 14, 14); /* vegas */
+       giza_set_colour_representation_rgb (3, 32, 133, 133); /* eucalyptus */
+       giza_set_colour_representation_rgb (4, 207, 62, 156);  /* parrot */
+       giza_set_colour_representation_rgb (5, 247, 74, 0); /* enjoyable orange */
+       giza_set_colour_representation_rgb (6, 38, 51, 17); /* cataclysm */
+       giza_set_colour_representation_rgb (7, 5, 79, 129); /* wintersky tango */
+       giza_set_colour_representation_rgb (8, 249, 223, 11); /* desert sun */
+       nlinecolours = 9;
+       break;
+    case 5: /* Colourblind-safe palette from http://jfly.iam.u-tokyo.ac.jp/color/ */
+       giza_set_colour_representation (2, 0.9, 0.6, 0.); /* orange */
+       giza_set_colour_representation (3, 0.35, 0.7, 0.9); /* sky blue */
+       giza_set_colour_representation (4, 0., 0.6, 0.5);  /* bluish green */
+       giza_set_colour_representation (5, 0.95, 0.9, 0.25); /* yellow */
+       giza_set_colour_representation (6, 0., 0.45, 0.7); /* blue */
+       giza_set_colour_representation (7, 0.8, 0.4, 0.); /* vermilion */
+       giza_set_colour_representation (8, 0.8, 0.6, 0.7); /* reddish purple */
+       nlinecolours = 9;
        break;
     default:
        giza_set_colour_representation (0, 1., 1., 1.);	/* white */
