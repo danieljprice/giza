2bca75fdf40dd2b74182210d99351b3f86edf51b
danielprice <danielprice@952f94de-64c9-43ce-8081-c12abd9885bf>
2014-06-17 23:29:56 +0000

    giza_histogram implemented; compatible with PGHIST
    git-svn-id: https://svn.code.sf.net/p/giza/code@433 952f94de-64c9-43ce-8081-c12abd9885bf

--- src/giza-histogram.c
+++ src/giza-histogram.c
@@ -31,22 +31,84 @@
  * Synopsis: Plot a histogram (unbinned)
  *
  * Input:
- *  -n    :- number of sub-panels in x direction
- *  -dat  :- number of sub panels in y direction
- *  -min  :- minimum
- *  -max  :- maximum
+ *  -n    :- number of data values
+ *  -dat  :- data (n values)
+ *  -min  :- minimum data value to use in histogram
+ *  -max  :- maximum data value to use in histogram
  *  -nbin :- number of bins
  *  -flag :- flag to indicate page changes, see below
  *
  * See also: giza_histogram_binned, giza_histogram_float
+ *
+ *  Flag:
+ *   -0   :- giza_histogram calls giza_environment to set up new plotting page
+ *   -1   :- plot in current window and viewport
+ *   -2   :- with filled area style, calls giza_environment
+ *   -3   :- with filled area style, in current window and viewport
+ *   -4   :- bins drawn as simple outlines, as in giza_histogram_binned, calls giza_environment
+ *   -5   :- bins drawn as simple outlines, but in current window and viewport
+ *  Other values treated as 1
  *
  */
 void
 giza_histogram (int n, const double *dat, double min, double max, int nbin, int flag)
 {
   if (!_giza_check_device_ready ("giza_histogram"))
-    return;  
- 
+    return;
+
+  int i,ibin;
+  int nmax = 0;
+  double bin_width = (max - min)/((double) nbin);
+  int ninbin[nbin];
+  for (ibin=0;ibin<nbin;ibin++)
+    {
+      ninbin[ibin] = 0;
+    }
+  
+  for (i=0;i<n;i++) 
+    {
+      double dati = dat[i];
+      ibin = (int) ((dat[i] - min)/bin_width);
+      if (ibin >= 0 && ibin < nbin)
+        {
+          ninbin[ibin] += 1;
+          if (ninbin[ibin] > nmax) nmax = ninbin[ibin];
+        }
+    }
+
+  double xmin,xmax,ymin,ymax;
+  int nval,oldFill;
+  ymax = nmax;
+  ymax = giza_round(1.01*ymax, &nval);
+  giza_get_fill(&oldFill);
+  
+  if (flag % 2 == 0) giza_set_environment(min,max,0.,(double) ymax,0,0);
+  
+  /* set up plotting environment and options */
+  switch (flag) 
+    {
+    case 5:
+    default:
+      giza_set_fill(GIZA_FILL_HOLLOW);
+      break;
+    case 3:
+      giza_set_fill(GIZA_FILL_SOLID);
+      break;
+    }
+
+  /* plot the bars of the histogram */
+  for (ibin=0;ibin<nbin;ibin++) 
+    {
+       xmin = min + ibin*bin_width;
+       xmax = xmin + bin_width;
+       ymin = 0.;
+       ymax = (double) ninbin[ibin];
+
+       giza_rectangle(xmin,xmax,ymin,ymax);
+    }
+
+  /* restore previous fill style */
+  giza_set_fill(oldFill);  
 }
 
 /**
