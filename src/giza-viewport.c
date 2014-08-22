9f9eb6bf235f2d15d735728cbad37b0bcf9fdf50
danielprice <danielprice@952f94de-64c9-43ce-8081-c12abd9885bf>
2014-08-22 10:42:12 +0000

    better margins in PGENV/giza_environment
    git-svn-id: https://svn.code.sf.net/p/giza/code@446 952f94de-64c9-43ce-8081-c12abd9885bf

--- src/giza-viewport.c
+++ src/giza-viewport.c
@@ -253,8 +253,8 @@ _giza_get_default_viewport (double *xmin, double *xmax, double *ymin, double *ym
 {
   double xch, ych;
   giza_get_character_size(GIZA_UNITS_NORMALIZED, &xch, &ych);
-  *xmin = 4.*xch;
-  *xmax = *xmin + (1. - 8.*xch);
+  *xmin = 4.*ych;
+  *xmax = *xmin + (1. - 8.*ych);
   *ymin = 4.*ych;
   *ymax = *ymin + (1. - 8.*ych);
 
