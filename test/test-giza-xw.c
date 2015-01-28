d8c464cb7cfdfaaa9b622000ab6744bd1197180a
danielprice <danielprice@952f94de-64c9-43ce-8081-c12abd9885bf>
2015-01-28 23:09:37 +0000

    renamed test-xw.c->test-giza-xw.c (thanks to Joachim Frieben)
    git-svn-id: https://svn.code.sf.net/p/giza/code@465 952f94de-64c9-43ce-8081-c12abd9885bf

--- test/test-giza-xw.c
+++ test/test-giza-xw.c
@@ -26,11 +26,11 @@
 int main() {
  int ierr;
  ierr = giza_open_device("/xw","test");
- giza_stop_prompting();
+ /*giza_stop_prompting();*/
  giza_close_device();
 
  ierr = giza_open_device("/xw","test");
- giza_stop_prompting();
+ /*giza_stop_prompting();*/
  giza_close_device();
 
  return ierr;
