76d06ff0c00baeaadd094eaaae60f4f7dd3ab857
Daniel Price <daniel.price@monash.edu>
2024-12-02 13:19:44 +1100

    (testsuite) added checks that files are successfully created for png, svg and pdf drivers

--- test/C/test-svg.c
+++ test/C/test-svg.c
@@ -23,11 +23,16 @@
  */
 #include <giza.h>
 #include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 static double sinx(double *x);
+static int check_svg_files(int num_files);
 
 int main() {
- int ierr, i;
- ierr = giza_open_device("/svg","test");
+ int id, i;
+ id = giza_open_device("/svg","test");
  for (i=1;i<=10;i++) {
      giza_set_environment(0.,1.,-1.,1.,0,0);
      giza_label("x","y","title");
@@ -35,9 +40,33 @@ int main() {
  }
  giza_close_device();
 
- return ierr;
+ /* open_device should return a positive integer */
+ if (id <= 0 || !check_svg_files(10)) {
+   return 1;
+ } else {
+   return 0;
+ }
 }
 
 double sinx(double *x) {
    return sin(6.28*(*x));
+}
+
+/* check if the corresponding files exist */
+int check_svg_files(int num_files) {
+    char filename[20];
+
+    for (int i = 0; i < num_files; i++) {
+        /* Create the filename in the format test_0000.svg, test_0001.svg, etc. */
+        snprintf(filename, sizeof(filename), "test_%04d.svg", i);
+
+        /* Check if the file exists */
+        if (access(filename, F_OK) == -1) {
+            fprintf(stderr, "Error: Required file %s does not exist.\n", filename);
+            return 0; /* Return 0 if any file is missing */
+        } else {
+            fprintf(stdout, "File %s exists -> OK\n", filename);
+        }
+    }
+    return 1; /* Return 1 if all files exist */
 }
